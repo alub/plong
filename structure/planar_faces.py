c43f6ab51b13a5b9824d114def9acf6da542e5fd
Vincent Duvert <vincent@duvert.net>
2012-02-11 13:01:46 +0100

    Planar faces are now checked for validity (i.e. is the defined plane not cutting the object in half?).

--- structure/planar_faces.py
+++ structure/planar_faces.py
@@ -4,16 +4,24 @@ MAX_ANGLE = .01
 ALIGN_TOLERANCE = .1
 
 class PlanarFacesSet(object):
-    def __init__(self, initial_face):
+    def __init__(self, obj, initial_face):
+        self._obj = obj
+        self._normal = initial_face.normal
         self._comp_face = initial_face
         self._proj_dist = initial_face.normal.dot(initial_face.center)
         self.total_area = initial_face.area
         self.faces = {initial_face}
     
     def test_face(self, face):
-        if self._comp_face.normal.angle(face.normal) <= MAX_ANGLE:
+        """
+        Checks if the given face should be in the set, and adds
+        it if it the case.
+        """
+        
+        
+        if self._normal.angle(face.normal) <= MAX_ANGLE:
             if face not in self.faces:
-                proj = self._comp_face.normal.dot(face.center)
+                proj = self._normal.dot(face.center)
                 if abs(self._proj_dist - proj) / abs(self._proj_dist) < ALIGN_TOLERANCE:
                     self.faces.add(face)
                     self.total_area += face.area
@@ -23,9 +31,35 @@ class PlanarFacesSet(object):
         return False
     
     def select_all(self):
+        """
+        Select all faces from the plane.
+        """
+        
         for face in self.faces:
-            print(face)
             face.select = True
+    
+    def get_plane(self):
+        """
+        Returns a vector and a point defining the plane containing
+        the faces.
+        """
+        
+        points = set()
+        for face in self.faces:
+            points.update(face.vertices)
+        
+        vertices = self._obj.data.vertices
+        
+        chosen_point = points.pop()
+        best_distance = self._normal.dot(vertices[chosen_point].co)
+        
+        for point in points:
+            distance = self._normal.dot(vertices[point].co)
+            if distance > best_distance:
+                chosen_point = point
+                best_distance = distance
+        
+        return self._normal, point
     
     def __repr__(self):
         return "<PlanarFacesSet: %r>" % self.faces
@@ -36,28 +70,53 @@ def cluster_planar_faces(obj):
     the same plane.
     """
     
-    result = []
+    face_sets = []
     remaining_faces = list(obj.data.faces)
     
     while remaining_faces:
         current_face = remaining_faces.pop()
         
         set_found = False
-        for face_set in result:
+        for face_set in face_sets:
             if face_set.test_face(current_face):
                 set_found = True
                 break
         
         if not set_found:
-            result.append(PlanarFacesSet(current_face))
+            face_sets.append(PlanarFacesSet(obj, current_face))
+    
+    valid_face_sets = []
+    
+    for face_set in face_sets:
+        vector, point = face_set.get_plane()
+        
+        ref_proj = vector.dot(obj.data.vertices[point].co)
+        is_valid = True
+        
+        for obj_point in obj.data.vertices:
+            proj = vector.dot(obj_point.co)
+            if proj > ref_proj:
+                if proj and abs(proj - ref_proj) / abs(proj) > .001:
+                    is_valid = False
+                    break
+        
+        if is_valid:
+            valid_face_sets.append(face_set)
+    
 
-    return sorted(result, key=lambda face_set:
+    return sorted(valid_face_sets, key=lambda face_set:
         face_set.total_area, reverse=True)
 
 if __name__ == '__main__':
     bpy.ops.object.mode_set(mode='EDIT')
     bpy.ops.mesh.select_all(action='DESELECT')
     bpy.ops.object.mode_set(mode='OBJECT')
-    face_sets = cluster_planar_faces(bpy.data.objects["Cube.001"])
-    face_sets[8].select_all()
+    
+    obj = bpy.data.objects["Eggshape"]
+    
+    face_sets = cluster_planar_faces(obj)
+    face_sets[9].select_all()
+    #obj.data.vertices[point].select = True
+    #print(vector)
+    
     bpy.ops.object.mode_set(mode='EDIT')
