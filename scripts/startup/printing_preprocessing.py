11f0268b76e4cab97115b05b14e1944b9b758802
Caroline Naud <crlnnaud00@gmail.com>
2012-03-06 17:14:43 +0100

    pb de la mauvaise face selectionnee resolu  enter the commit message for your changes. Lines starting

--- scripts/startup/printing_preprocessing.py
+++ scripts/startup/printing_preprocessing.py
@@ -215,6 +215,7 @@ class FindSupportingPlanesOperator(bpy.types.Operator) :
         
         obj = bpy.context.active_object
         sp = planar_faces.SupportPlanes(obj)
+        plane = 0
         sp[0].select()
         self.report({"INFO"}, "Several supporting planes have been found for your object")
         step = 3 # Disable the second box and enable the third one
